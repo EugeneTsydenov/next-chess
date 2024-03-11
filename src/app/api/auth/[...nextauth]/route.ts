8d5b3cf6f10eb77e2be59c8bdc16a30dfd23ffb1
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-03-11 19:33:03 +0800

    add: config next auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,29 @@
+import CredentialsProvider from 'next-auth/providers/credentials';
+import NextAuth from 'next-auth';
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'text',
+          placeholder: 'Enter your email',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'Enter your password',
+        },
+      },
+      async authorize(credentials) {
+        return null;
+      },
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
