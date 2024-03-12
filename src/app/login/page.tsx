6279df11a004f9752c51297a7256026e96bf1479
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-03-12 16:51:51 +0800

    fix: params types in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,9 +3,11 @@ import * as React from 'react';
 
 interface LoginProps {
   callbackUrl?: string;
+  searchParams: Record<string, string>;
 }
 
 const Login: React.FC<LoginProps> = props => {
+  console.log(props);
   return <LoginPage searchParams={props} />;
 };
 
