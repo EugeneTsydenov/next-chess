21c699cd7b2427e198410f12d2bf116caa63787e
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-03-12 16:37:22 +0800

    add: session provider next auth

--- src/app/_providers/Providers.tsx
+++ src/app/_providers/Providers.tsx
@@ -1,8 +1,13 @@
 import ThemeProvider from '@/app/_providers/ThemeProvider';
+import { SessionProvider } from 'next-auth/react';
 import * as React from 'react';
 
 const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <SessionProvider>
+      <ThemeProvider>{children}</ThemeProvider>
+    </SessionProvider>
+  );
 };
 
 export default Providers;
