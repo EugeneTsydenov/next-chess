4d71f3daae58912ee231a422ac9d3bf2585a8585
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-03-27 21:15:00 +0800

    change: navbar path

--- src/widgets/header/lib/data/NavLinks.ts
+++ src/widgets/header/lib/data/NavLinks.ts
@@ -1,87 +1,97 @@
 import { IDropDawnLink, INavBarLink } from '@/widgets/header/model/types/INavLinks';
 import { v4 as uuidv4 } from 'uuid';
 
-const playDropDawnLinks: IDropDawnLink[] = (function () {
+function playDropDawnLinks(parentPath: string): IDropDawnLink[] {
   return [
-    { id: uuidv4(), dropDownLinkName: 'Online', dropDownPath: '/online' },
-    { id: uuidv4(), dropDownLinkName: 'Offline', dropDownPath: '/offline' },
-    { id: uuidv4(), dropDownLinkName: 'Against bot', dropDownPath: '/bot' },
-    { id: uuidv4(), dropDownLinkName: 'Against friend', dropDownPath: '/friend' },
+    { id: uuidv4(), dropDownLinkName: 'Online', dropDownPath: `${parentPath}/online` },
+    { id: uuidv4(), dropDownLinkName: 'Offline', dropDownPath: `${parentPath}/offline` },
+    { id: uuidv4(), dropDownLinkName: 'Against bot', dropDownPath: `${parentPath}/bot` },
+    { id: uuidv4(), dropDownLinkName: 'Against friend', dropDownPath: `${parentPath}/friend` },
   ];
-})();
+}
 
-const tasksDropDawnLinks: IDropDawnLink[] = (function () {
+function tasksDropDawnLinks(parentPath: string): IDropDawnLink[] {
   return [
-    { id: uuidv4(), dropDownLinkName: 'Create a task', dropDownPath: '/#' },
-    { id: uuidv4(), dropDownLinkName: 'Puzzle Streak', dropDownPath: '/#' },
-    { id: uuidv4(), dropDownLinkName: 'Puzzle Storm', dropDownPath: '/#' },
-    { id: uuidv4(), dropDownLinkName: 'Puzzle Racer', dropDownPath: '/#' },
+    { id: uuidv4(), dropDownLinkName: 'Create a task', dropDownPath: `${parentPath}/#` },
+    { id: uuidv4(), dropDownLinkName: 'Puzzle Streak', dropDownPath: `${parentPath}/#` },
+    { id: uuidv4(), dropDownLinkName: 'Puzzle Storm', dropDownPath: `${parentPath}/#` },
+    { id: uuidv4(), dropDownLinkName: 'Puzzle Racer', dropDownPath: `${parentPath}/#` },
   ];
-})();
+}
 
-const educationDropDawnLinks: IDropDawnLink[] = (function () {
+function educationDropDawnLinks(parentPath: string): IDropDawnLink[] {
   return [
-    { id: uuidv4(), dropDownLinkName: 'Chess basics', dropDownPath: '/#' },
-    { id: uuidv4(), dropDownLinkName: 'Practice', dropDownPath: '/#' },
-    { id: uuidv4(), dropDownLinkName: 'Coordinates', dropDownPath: '/#' },
-    { id: uuidv4(), dropDownLinkName: 'Studio', dropDownPath: '/#' },
-    { id: uuidv4(), dropDownLinkName: 'Trainers', dropDownPath: '/#' },
+    { id: uuidv4(), dropDownLinkName: 'Chess basics', dropDownPath: `${parentPath}/#` },
+    { id: uuidv4(), dropDownLinkName: 'Practice', dropDownPath: `${parentPath}/#` },
+    { id: uuidv4(), dropDownLinkName: 'Coordinates', dropDownPath: `${parentPath}/#` },
+    { id: uuidv4(), dropDownLinkName: 'Studio', dropDownPath: `${parentPath}/#` },
+    { id: uuidv4(), dropDownLinkName: 'Trainers', dropDownPath: `${parentPath}/#` },
   ];
-})();
+}
 
-const viewingDropDawnLinks: IDropDawnLink[] = (function () {
+function viewingDropDawnLinks(parentPath: string): IDropDawnLink[] {
   return [
-    { id: uuidv4(), dropDownLinkName: 'ChessHub TV', dropDownPath: '/#' },
-    { id: uuidv4(), dropDownLinkName: 'Current games', dropDownPath: '/#' },
-    { id: uuidv4(), dropDownLinkName: 'Translations', dropDownPath: '/#' },
-    { id: uuidv4(), dropDownLinkName: 'Video library', dropDownPath: '/#' },
+    { id: uuidv4(), dropDownLinkName: 'ChessHub TV', dropDownPath: `${parentPath}/#` },
+    { id: uuidv4(), dropDownLinkName: 'Current games', dropDownPath: `${parentPath}/#` },
+    { id: uuidv4(), dropDownLinkName: 'Translations', dropDownPath: `${parentPath}/#` },
+    { id: uuidv4(), dropDownLinkName: 'Video library', dropDownPath: `${parentPath}/#` },
   ];
-})();
+}
 
-const communityDropDawnLinks: IDropDawnLink[] = (function () {
+function communityDropDawnLinks(parentPath: string): IDropDawnLink[] {
   return [
-    { id: uuidv4(), dropDownLinkName: 'Players', dropDownPath: '/#' },
-    { id: uuidv4(), dropDownLinkName: 'Clubs', dropDownPath: '/#' },
-    { id: uuidv4(), dropDownLinkName: 'Forum', dropDownPath: '/#' },
-    { id: uuidv4(), dropDownLinkName: 'Blog', dropDownPath: '/#' },
+    { id: uuidv4(), dropDownLinkName: 'Players', dropDownPath: `${parentPath}/#` },
+    { id: uuidv4(), dropDownLinkName: 'Clubs', dropDownPath: `${parentPath}/#` },
+    { id: uuidv4(), dropDownLinkName: 'Forum', dropDownPath: `${parentPath}/#` },
+    { id: uuidv4(), dropDownLinkName: 'Blog', dropDownPath: `${parentPath}/#` },
   ];
-})();
+}
 
-const instrumentsDropDawnLinks: IDropDawnLink[] = (function () {
+function instrumentsDropDawnLinks(parentPath: string): IDropDawnLink[] {
   return [
-    { id: uuidv4(), dropDownLinkName: 'Analyze the game', dropDownPath: '/#' },
-    { id: uuidv4(), dropDownLinkName: 'Debuts', dropDownPath: '/#' },
-    { id: uuidv4(), dropDownLinkName: 'Board editor', dropDownPath: '/#' },
-    { id: uuidv4(), dropDownLinkName: 'Import batch', dropDownPath: '/#' },
-    { id: uuidv4(), dropDownLinkName: 'Advanced search', dropDownPath: '/#' },
+    { id: uuidv4(), dropDownLinkName: 'Analyze the game', dropDownPath: `${parentPath}/#` },
+    { id: uuidv4(), dropDownLinkName: 'Debuts', dropDownPath: `${parentPath}/#` },
+    { id: uuidv4(), dropDownLinkName: 'Board editor', dropDownPath: `${parentPath}/#` },
+    { id: uuidv4(), dropDownLinkName: 'Import batch', dropDownPath: `${parentPath}/#` },
+    { id: uuidv4(), dropDownLinkName: 'Advanced search', dropDownPath: `${parentPath}/#` },
   ];
-})();
+}
 
 export const navLinks: INavBarLink[] = [
-  { id: uuidv4(), navLinkName: 'Play', navPath: '/play', dropDownLinks: playDropDawnLinks },
-  { id: uuidv4(), navLinkName: 'Tasks', navPath: '/tasks', dropDownLinks: tasksDropDawnLinks },
+  {
+    id: uuidv4(),
+    navLinkName: 'Play',
+    navPath: '/play',
+    dropDownLinks: playDropDawnLinks('/play'),
+  },
+  {
+    id: uuidv4(),
+    navLinkName: 'Tasks',
+    navPath: '/tasks',
+    dropDownLinks: tasksDropDawnLinks('/tasks'),
+  },
   {
     id: uuidv4(),
     navLinkName: 'Education',
     navPath: '/education',
-    dropDownLinks: educationDropDawnLinks,
+    dropDownLinks: educationDropDawnLinks('/education'),
   },
   {
     id: uuidv4(),
     navLinkName: 'Viewing',
     navPath: '/viewing',
-    dropDownLinks: viewingDropDawnLinks,
+    dropDownLinks: viewingDropDawnLinks('/viewing'),
   },
   {
     id: uuidv4(),
     navLinkName: 'Community',
     navPath: '/community',
-    dropDownLinks: communityDropDawnLinks,
+    dropDownLinks: communityDropDawnLinks('/community'),
   },
   {
     id: uuidv4(),
     navLinkName: 'Instruments',
     navPath: '/instruments',
-    dropDownLinks: instrumentsDropDawnLinks,
+    dropDownLinks: instrumentsDropDawnLinks('/instruments'),
   },
 ];
