c60241280dcea439909733b06821d309b9986bd9
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-03-27 13:56:30 +0800

    add: base query and base api for default and private apis

---
+++ src/shared/api/client-side/privateApi.ts
@@ -0,0 +1,8 @@
+import { createApi } from '@reduxjs/toolkit/query/react';
+import { privateQuery } from '@/shared/config';
+
+export const privateApi = createApi({
+  reducerPath: 'privateApi',
+  baseQuery: privateQuery,
+  endpoints: () => ({}),
+});
