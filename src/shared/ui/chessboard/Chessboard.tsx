b247e68e38f95c9841134dc4850649387f1fa562
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-03-31 21:17:39 +0800

    change: ui chessboard props

--- src/shared/ui/chessboard/Chessboard.tsx
+++ src/shared/ui/chessboard/Chessboard.tsx
@@ -1,13 +1,62 @@
 'use client';
 
+import { ChessboardProps } from 'react-chessboard/dist/chessboard/types';
 import { Chessboard as ReactChessboard } from 'react-chessboard';
+import { ForwardRefExoticComponent } from 'react';
 import * as React from 'react';
 
-interface ChessboardProps {
-  boardWidth: number;
-}
-
-const Chessboard: React.FC<ChessboardProps> = ({ boardWidth }) => {
+const Chessboard: React.FC<
+  ForwardRefExoticComponent<
+    Pick<
+      ChessboardProps,
+      | 'allowDragOutsideBoard'
+      | 'animationDuration'
+      | 'areArrowsAllowed'
+      | 'arePiecesDraggable'
+      | 'arePremovesAllowed'
+      | 'autoPromoteToQueen'
+      | 'boardOrientation'
+      | 'boardWidth'
+      | 'clearPremovesOnRightClick'
+      | 'customArrows'
+      | 'customArrowColor'
+      | 'customBoardStyle'
+      | 'customNotationStyle'
+      | 'customDarkSquareStyle'
+      | 'customDndBackend'
+      | 'customDndBackendOptions'
+      | 'customDropSquareStyle'
+      | 'customLightSquareStyle'
+      | 'customPieces'
+      | 'customPremoveDarkSquareStyle'
+      | 'customPremoveLightSquareStyle'
+      | 'customSquare'
+      | 'customSquareStyles'
+      | 'dropOffBoardAction'
+      | 'id'
+      | 'isDraggablePiece'
+      | 'getPositionObject'
+      | 'onArrowsChange'
+      | 'onDragOverSquare'
+      | 'onMouseOutSquare'
+      | 'onMouseOverSquare'
+      | 'onPieceClick'
+      | 'onPieceDragBegin'
+      | 'onPieceDragEnd'
+      | 'onPieceDrop'
+      | 'onPromotionCheck'
+      | 'onPromotionPieceSelect'
+      | 'onSquareClick'
+      | 'onSquareRightClick'
+      | 'position'
+      | 'promotionDialogVariant'
+      | 'promotionToSquare'
+      | 'showBoardNotation'
+      | 'showPromotionDialog'
+      | 'snapToCursor'
+    >
+  >
+> = props => {
   return (
     <ReactChessboard
       customBoardStyle={{
@@ -16,7 +65,7 @@ const Chessboard: React.FC<ChessboardProps> = ({ boardWidth }) => {
       }}
       customDarkSquareStyle={{ backgroundColor: '#779952' }}
       customLightSquareStyle={{ backgroundColor: '#edeed1' }}
-      boardWidth={boardWidth}
+      {...props}
     />
   );
 };
