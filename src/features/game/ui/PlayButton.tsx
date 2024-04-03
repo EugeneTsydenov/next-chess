2f30de0c31ca05bee781bb69374c08d4af1828c2
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-04-03 15:27:33 +0800

    fix

--- src/features/game/ui/PlayButton.tsx
+++ src/features/game/ui/PlayButton.tsx
@@ -1,21 +1,11 @@
 'use client';
 
-import { testSocket } from '@/entities/game/api/socket';
 import { Button, Typography } from '@mui/material';
 import * as React from 'react';
 
 const PlayButton: React.FC = () => {
-  function handleClick() {
-    testSocket();
-  }
-
   return (
-    <Button
-      fullWidth
-      variant='contained'
-      sx={{ p: 1.5, borderRadius: '5px' }}
-      onClick={handleClick}
-    >
+    <Button fullWidth variant='contained' sx={{ p: 1.5, borderRadius: '5px' }}>
       <Typography variant='h4' fontWeight={600}>
         Play
       </Typography>
