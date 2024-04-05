ff0459a4132365381b79b287366f07bf9ba8d49c
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-04-05 20:54:26 +0800

    imlement: draggable piece

--- src/entities/game/ui/GameChessboard.tsx
+++ src/entities/game/ui/GameChessboard.tsx
@@ -2,9 +2,11 @@
 
 import {
   fenSelector,
+  turnSelector,
   userRoleSelector,
   userSideSelector,
 } from '@/entities/game/model/redux/gameSelectors';
+import { Piece, Square } from 'react-chessboard/dist/chessboard/types';
 import { useAppSelector } from '@/shared/lib';
 import { Chessboard } from '@/shared/ui';
 import * as React from 'react';
@@ -13,14 +15,20 @@ const GameChessboard: React.FC = () => {
   const fen = useAppSelector(fenSelector);
   const userSide = useAppSelector(userSideSelector);
   const userRole = useAppSelector(userRoleSelector);
+  const turn = useAppSelector(turnSelector);
+
+  function isDraggablePiece({ piece }: { piece: Piece }) {
+    if (userRole === 'watcher') return false;
+    if (userSide !== turn) return false;
+    if (userSide !== piece[0]) return false;
+    return true;
+  }
 
   return (
     <Chessboard
       boardWidth={560}
       position={fen}
-      isDraggablePiece={() => {
-        return userRole !== 'watcher';
-      }}
+      isDraggablePiece={isDraggablePiece}
       boardOrientation={userSide === 'w' ? 'white' : 'black'}
     />
   );
