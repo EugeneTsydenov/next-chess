814bfe6d37e59f984696a6d92bc2a47b7a0691d9
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-04-05 20:55:34 +0800

    add: find game

--- src/features/game/ui/PlayButton.tsx
+++ src/features/game/ui/PlayButton.tsx
@@ -1,11 +1,28 @@
 'use client';
 
+import { gameSocket, timeSelector } from '@/entities/game';
 import { Button, Typography } from '@mui/material';
+import { useAppSelector } from '@/shared/lib';
+import { jwtSelector } from '@/shared/model';
 import * as React from 'react';
 
-const PlayButton: React.FC = () => {
+interface PlayButtonProps {
+  gameMode: 'online' | 'friend' | 'bot' | 'offline';
+}
+
+const PlayButton: React.FC<PlayButtonProps> = ({ gameMode }) => {
+  const timeMode = useAppSelector(timeSelector);
+  const jwt = useAppSelector(jwtSelector);
+
   return (
-    <Button fullWidth variant='contained' sx={{ p: 1.5, borderRadius: '5px' }}>
+    <Button
+      fullWidth
+      variant='contained'
+      sx={{ p: 1.5, borderRadius: '5px' }}
+      onClick={() => {
+        gameSocket.findGame(jwt!, { timeMode: timeMode.timeTitle });
+      }}
+    >
       <Typography variant='h4' fontWeight={600}>
         Play
       </Typography>
