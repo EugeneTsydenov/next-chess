66be5c774459cb51532529ac22fe9abfd2e556e1
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-04-05 20:49:11 +0800

    add: conctant base api url

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -4,3 +4,4 @@ export { useAppSelector } from './hooks/useAppSelector';
 export { Http } from './http/http';
 export { useWindow } from './hooks/useWindow';
 export { useToggle } from './hooks/useToggle';
+export { BASE_API_URL } from './constants/constants';
